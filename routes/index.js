7d7ffa2489631ded9c2ffa494012a9c36667ebb8
andreESGI <andre92.morel@gmail.com>
2018-04-25 23:16:44 +0200

    Revert "display values + formulaire"
    This reverts commit 78e5a87c9f0c9fd4864b475516491c8c62c4a5af.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ module.exports = function(app, urlApi, urlLocal, utils) {
 
 	//FRONT
 	require("./accueil")(app, urlApi);
-	require("./annonce")(app, urlApi);
 	require("./inscription")(app, urlApi, urlLocal, utils);
 	require("./connexion")(app, urlApi, utils);
 	require("./validationInscription")(app, urlApi);
