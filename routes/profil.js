e4313aea33e9a7f482ff9c68f74e659ac0c5a7ea
iPlowPlow <goasguen.loic@gmail.com>
2018-04-30 22:44:58 +0200

    ajout informationLivraison (nom, prenom, adresse ...)

--- routes/profil.js
+++ routes/profil.js
@@ -224,12 +224,139 @@ module.exports = function(app, urlApi, urlLocal, utils){
         }
     });
 
+    app.post("/informationsLivraison", function (req, res, next) {
+        if(!req.session.type) {
+			res.redirect("/");
+		}else{
+            var profil = getLocalProfil(req);
+            
+            if(!req.body.lastName) {
+                res.render("profil.ejs", {
+                    session: req.session,
+                    profil: profil,
+                    msgError:"Veuillez saisir un nom !",
+                    msgSuccess: ""
+                });
+            }else if(!req.body.firstName) {
+                res.render("profil.ejs", {
+                    session: req.session,
+                    profil: profil,
+                    msgError:"Veuillez saisir un prénom !",
+                    msgSuccess: ""
+                });
+            }else if(!req.body.birth) {
+                res.render("profil.ejs", {
+                    session: req.session,
+                    profil: profil,
+                    msgError:"Veuillez saisir une date de naissance !",
+                    msgSuccess: ""
+                });
+            }else if(!req.body.sex) {
+                res.render("profil.ejs", {
+                    session: req.session,
+                    profil: profil,
+                    msgError:"Veuillez saisir un sexe !",
+                    msgSuccess: ""
+                });
+            }else if(!req.body.address) {
+                res.render("profil.ejs", {
+                    session: req.session,
+                    profil: profil,
+                    msgError:"Veuillez saisir une adresse !",
+                    msgSuccess: ""
+                });
+            }else if(!req.body.city) {
+                res.render("profil.ejs", {
+                    session: req.session,
+                    profil: profil,
+                    msgError:"Veuillez saisir une ville !",
+                    msgSuccess: ""
+                });
+            }
+            else if(!req.body.cp) {
+                res.render("profil.ejs", {
+                    session: req.session,
+                    profil: profil,
+                    msgError:"Veuillez saisir un code postal !",
+                    msgSuccess: ""
+                });
+            
+            }else{
+                
+                rp({
+                    url: urlApi + "/user/update",
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    json: {
+                        "loginUser" : req.session.login,
+                        "lastNameUser": req.body.lastName,
+                        "firstNameUser": req.body.firstName,
+                        "birthUser": req.body.birth,
+                        "sexUser": req.body.sex,
+                        "addressUser": req.body.address,
+                        "cityUser": req.body.city,
+                        "cpUser": req.body.cp
+                    }
+                }).then(function(body) {    
+                    if(body.code ==0){
+
+                        profil.firstNameUser = req.body.lastName;
+                        profil.lastNameUser = req.body.firstName;
+                        profil.birthUser = req.body.birth;
+                        profil.sexUser = req.body.sex;
+                        profil.addressUser = req.body.address;
+                        profil.cityUser = req.body.city;
+                        profil.cpUser = req.body.cp;
+
+                        res.render("profil.ejs", {
+                            session: req.session,
+                            profil: profil,
+                            msgError:"",
+                            msgSuccess: "Modification des informations de livraison effectuée !"
+                           
+                        });
+                    }else{
+                        
+                        res.render("profil.ejs", {
+                            session: req.session,
+                            profil: profil,
+                            msgError:"Erreur lors de la modification, veuillez recommmencer",
+                            msgSuccess: ""
+                           
+                        });
+                      
+                    }
+                }).catch(function (err) {
+                    console.log(err)
+
+                    res.render("profil.ejs", {
+                        session: req.session,
+                        profil: profil,
+                        msgError:"Erreur lors de la modification, veuillez recommmencer",
+                        msgSuccess: ""
+                    });
+                });
+
+            }
+
+
+        }
+    });
+
 
     function getLocalProfil(req){
         var profil = {
             emailUser : req.body.profilMail,
-            loginUser : req.session.login
-
+            loginUser : req.session.login,
+            firstNameUser : req.body.profilFirstName,
+            lastNameUser : req.body.profilLastName,
+            birthUser : req.body.profilBirth,
+            sexUser : req.body.profilSex,
+            addressUser : req.body.profilAddress,
+            cityUser : req.body.profilCity,
+            cpUser : req.body.profilCp
         };
         return profil;
     }
