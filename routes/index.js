2138f3f25c1c36dc71eb3ee9f3cb3749018c9d30
andreESGI <andre92.morel@gmail.com>
2018-05-08 17:53:32 +0200

    feature finie (Jshint à faire + quelques ajustements)

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ module.exports = function(app, urlApi, urlLocal, utils) {
 
 	//FRONT
 	require("./accueil")(app, urlApi);
-	require("./annonce")(app, urlApi);
+	require("./item")(app, urlApi, utils);
 	require("./inscription")(app, urlApi, urlLocal, utils);
 	require("./connexion")(app, urlApi, utils);
 	require("./validationInscription")(app, urlApi);
 	require("./deconnexion")(app);
+	require("./product")(app, urlApi);
    
 };
---
+++ routes/product.js
@@ -0,0 +1,33 @@
+module.exports = function(app, urlApi){
+  
+ 
+	// =====================================
+	// SIGNUP ==============================
+	// =====================================
+	// show the signup form
+	
+	app.get('/product/getByCategoryId', function(req, res, next) {
+    var rp = require("request-promise");
+		if(req.query.id){
+			var msgError;
+			msgError="";
+			rp({
+				url: urlApi + "/products/findByCategoryId?id="+req.query.id,
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}).then(function (body) {
+				if (JSON.parse(body).code == "0") {
+					res.send(body);
+				} else {
+					res.send(null);
+				}
+			}).catch(function (err) {
+				console.log(err)
+				res.send(null);
+			});
+		}else res.send(null);
+  });
+    
+};
